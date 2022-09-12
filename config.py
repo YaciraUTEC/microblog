2b9a0368eaecdfe45505b05d7ace48843d2d438c
unknown <yacira.campoverde@utec.edu.pe>
2022-09-11 21:35:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+#Aquí se le dice a SQLAchemy como conectarse la base de datos
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     #coger nuestras variables de entorno (en .flaskenv) el URL de conexion para la base de datos
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    #environ: variable de entorno
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')or\
+         'postgresq1://postgres:UTEC@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
